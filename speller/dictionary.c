6cfdbfd815100fe6f2dc49a7b027d32b04bd3032
Daniel-Choiniere <dpchoiniere@gmail.com>
2019-07-01 23:41:47 +0000

    allocated memory for each node we need to create using Malloc. Created a counter variable to be able to keep track of the word count

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -20,6 +20,9 @@ node;
 // Represents a hash table
 node *hashtable[NUM_CONTAINERS];
 
+// creates a variable intialized to 0 that will keep track of thw nuber of words in dictionary
+int wordCount = 0;
+
 // Hashes word to a number between 0 and 25, inclusive, based on its first letter
 unsigned int hash(const char *word)
 {
@@ -49,7 +52,18 @@ bool load(const char *dictionary)
     // Insert words into hash table
     while (fscanf(file, "%s", word) != EOF)
     {
-        // TODO
+        node *createdNode1 = malloc(sizeof(node));
+        node *createdNode2 = malloc(sizeof(node));
+        
+        // strcpy inserts the "word" char into the word varaible by accesing it through arrow notation and setting the node value
+        strcpy(createdNode1->word, word);
+        
+        // sends "word" to the hash function to hash to a number between o and 25 inclusive
+        int hash_value = hash(word);
+        
+        
+        
+        
     }
 
     // Close dictionary
